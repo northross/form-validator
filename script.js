75ac402d73165aef441a5cae3dc80e2d2f2356dc
northross <rossnorthrup@yahoo.com>
2020-07-10 12:27:24 -0400

    README added

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+
 // Show inpupt error message
 function showError(input, message) {
   const formControl = input.parentElement;
@@ -10,11 +11,13 @@ function showError(input, message) {
   const small = formControl.querySelector('small');
   small.innerText = message;
 }
+
 // Show success outline
 function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
 }
+
 // Check email is valid
 function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -24,6 +27,7 @@ function checkEmail(input) {
     showError(input, 'Email is not valid');
   }
 }
+
 // check Require
 function checkRequired(inputArr) {
   inputArr.forEach(function (input) {
@@ -52,6 +56,12 @@ function checkLength(input, min, max) {
   }
 }
 
+//  Check password match
+function checkPasswords(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
+}
 //  Get field
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -63,4 +73,5 @@ form.addEventListener('submit', function (event) {
   checkLength(username, 3, 15);
   checkLength(password, 6, 25);
   checkEmail(email);
+  checkPasswords(password, password2);
 });
